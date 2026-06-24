7c209de846b03d691c7f8342de9e565a7c819169
theharshitcode <207443006+theharshitcode@users.noreply.github.com>
2026-06-24 18:52:23 +0530

    Fix files for versel deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { dom } from '@fortawesome/fontawesome-svg-core'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "@fontsource/inter";
+import { AuthProvider }
+from "./context/AuthContext";
+import "./index.css";
+import App from "./App";
 
-dom.watch();
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
